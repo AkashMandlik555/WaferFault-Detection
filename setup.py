b7842eb905742796ba0fbebbbc535aa9a42000b9
Akash Mandlik <akashmandlik555@gmail.com>
2023-09-03 02:09:21 +0530

    commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wafer-fault-detection",
+    version="0.0.1",
+    description="ML project",
+    author="Rohan Patankar", 
+    packages=find_packages(),
+ )
